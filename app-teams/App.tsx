2670c217851553f9ffb2d57b6ced2824c9f760cd
MAYAN KELWIN <64339637+mayankelwin@users.noreply.github.com>
2024-11-19 02:11:57 -0400

    19/11/24

---
+++ app-teams/App.tsx
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Groups,  } from './src/screens/Groups';
+
+export default function App() {
+  return (
+    <Groups />
+  );
+}
---
+++ app-teams/src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native"
+import { styles } from "./styles"
+
+export function Groups () {
+return (
+<View style={styles.container}>
+      <Text>Groups</Text>
+    </View>
+)
+}
